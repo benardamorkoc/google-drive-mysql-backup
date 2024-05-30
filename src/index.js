77acae1359802b9b3d32a37614719a83302e0d56
Arda Morkoç <123625632+benardamorkoc@users.noreply.github.com>
2024-05-31 01:42:31 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const generateMysqlDump = require('./lib/mysqldump_integration');
+const uploadFileOnGoogleDrive = require('./lib/google_drive_integration');
+
+/**
+ * Creates a MySQL database dump and uploads it to Google Drive.
+ * 
+ * @param {string} filename - The name of the file to upload.
+ * @param {string} privateKeyPath - The path to the private key JSON file.
+ * @param {Object} dbConfig - The database configuration.
+ * @param {Object} googleDriveConfig - The Google Drive configuration.
+ * @returns {Promise<void>}
+ */
+async function createAndUploadBackup(filename, privateKeyPath, dbConfig, googleDriveConfig) {
+    // Generate MySQL dump
+    generateMysqlDump(dbConfig.host, dbConfig.user, dbConfig.password, dbConfig.database, filename);
+
+    // Upload file to Google Drive
+    await uploadFileOnGoogleDrive(privateKeyPath, filename, googleDriveConfig.parentId, googleDriveConfig.deleteAfterUpload);
+}
+
+module.exports = createAndUploadBackup;
